348d8781b83b226a0006419ea70419d34631baaa
IceFox27 <erofey.karpov@gmail.com>
2026-03-22 16:50:31 +1000

    Добавлены миграции БД

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://icefox:27xx69alphaCoder@localhost:5532/myhousedv'
+    SECRET_KEY = '59287JFHIajgrgkn52ofspkvsbs4'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
---
+++ app/models/post.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from ..extensions import db
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    teacher = db.Column(db.String(250))
+    subject = db.Column(db.String(250))
+    student = db.Column(db.String(250))
