5297870d4f4e0170e86db64bc4cffc3801a7a5fc
IceFox27 <erofey.karpov@gmail.com>
2026-03-22 18:16:11 +1000

    Добавлен маршрут для главной страницы

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,12 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from ..extensions import db
 from ..models.post import Post
 
 post = Blueprint('post', __name__)
 
-@post.route('/post/<subject>')
-def create_subject(name):
-    post = Post(name=name)
-    db.session.add(post)
-    db.session.commit()
-    return 'Subject Created!'
+@post.route('/post/create', methods=['POST'])
+def create():
+    return render_template('post/create.html')
