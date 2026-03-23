3ccf1c3b6865619c5493d8db03b7b0458ef33eec
IceFox27 <erofey.karpov@gmail.com>
2026-03-23 21:44:58 +1000

    Добавлена таблица для работы с БД на главной странице

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,9 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from ..extensions import db
 from ..models.post import Post
 
 post = Blueprint('post', __name__)
 
-@post.route('/post/create', methods=['POST'])
+@post.route('/', methods=['POST', 'GET'])
+def all():
+    posts = Post.query.all()
+    return render_template('post/all.html', posts=posts)
+
+@post.route('/post/create', methods=['POST', 'GET'])
 def create():
-    return render_template('post/create.html')
+    if request.method == 'POST':
+        teacher = request.form.get('teacher')
+        subject = request.form.get('subject')
+        student = request.form.get('student')
+
+        post = Post(teacher=teacher, subject=subject, student=student)
+
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(str(e))        
+    else:
+        return render_template('post/create.html')
+
+@post.route('/post/<int:id>/update', methods=['POST', 'GET'])
+def update(id):
+    if request.method == 'POST':
+        pass      
+    else:
+        return render_template('post/update.html')
